fdf4f1c007eee1e2c84a270de76e4e5c3a884083
imran12357 <imrankhanansari12357@gmail.com>
2022-03-12 11:26:58 +0530

    file upload

--- routes/product.route.js
+++ routes/product.route.js
@@ -1,11 +1,30 @@
 const express =require('express');
 const  router = express.Router()
+const multer =require('multer')
+const fs= require('fs')
+const path= require('path')
+
 
 const productctrl =require('../controllers/product.ctrl')
 
+const dir = './uploads';
+if (!fs.existsSync(dir)) fs.mkdirSync(dir);
+
+const storage = multer.diskStorage({
+    destination: './uploads',
+    filename: function (req, file, cb) {
+        const uniqueToken = Date.now() + '-' + Math.round(Math.random() * 1E9);
+        const fileName = uniqueToken + '-' + file.originalname;
+        req.resume = fileName;
+        cb(null, fileName);
+    }
+});
+
+const upload = multer({ storage: storage });
+
 
 router.post('/',productctrl.add)
-router.put('/:id',productctrl.updateproduct)
+router.put('/:id',upload.single('image'),productctrl.updateproduct)
 router.get('/:id',productctrl.getproductbyid)
 router.get('/page/:page/size/:size',productctrl.Products)
 router.get('/',productctrl.Products)
